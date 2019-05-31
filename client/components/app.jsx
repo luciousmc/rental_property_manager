b7d70e346a8caca423bf0dc013e6dada85bdce7e
Marlon Clay <luciousmc@icloud.com>
2019-05-31 19:20:58 +0000

    added basic HTML render for landing page included bootstrap to index.html applied basic styles to see elemnts

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './header';
 
 export default class App extends React.Component{
     constructor(props){
         super(props);
-
+        this.state = {
+            view:{
+                name: 'landing',
+                params: null,
+            }
+        }
     }
     render(){
-        return(
-            <h1>HELLOOOOO OUT THERERR</h1>
-        );
+        if (this.state.view.name === 'landing'){
+            return(
+                <div className="container">
+                    <header className="row">
+                        <Header />
+                    </header>
+                    <main>
+                        <div className="main-container row">
+                            <div className="col-6 manager-container">
+                                
+                            </div>
+                            <div className="col-6 tenants-container">
+
+                            </div>
+                        </div>
+                    </main>
+                </div>
+            );
+        }
+        
     }
 }
