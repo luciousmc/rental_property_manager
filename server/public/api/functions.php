4e2e2ca7078c2bd59d35020af2cc1e66ea1c940c
Howard <qsshorty@gmail.com>
2019-05-31 22:13:06 +0000

    added response code and header

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,7 +1,16 @@
 <?php
 function error_handler($error){
-    print($error->getMessage());
+    $output = [
+        "success" => false,
+        "error" => $error -> getMessage()
+    ];
+    $json_output = json_encode($output);
+    print($json_output);
 }
 set_exception_handler('error_handler');
+http_response_code(500);
 
-
+function startup(){
+    header('Content-Type: application/json');
+}
+?>
