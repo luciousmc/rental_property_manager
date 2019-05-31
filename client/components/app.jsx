db1f09b41071be2cd9bd0199b333590b0548d5b9
Marlon Clay <luciousmc@icloud.com>
2019-05-31 17:02:23 +0000

    modified webpack config

---
+++ client/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+    render(){
+        return(
+            <h1>HELLOOOOO OUT THERERR</h1>
+        );
+    }
+}
