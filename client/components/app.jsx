489505bee39723d054cd4316460862a7ae16adf4
chrisg91322 <chrisg913@gmail.com>
2019-05-31 22:15:47 +0000

    Completed header component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
 
 export default class App extends React.Component{
-    constructor(props){
+    constructor(props) {
         super(props);
-
-    }
-    render(){
-        return(
-            <h1>HELLOOOOO OUT THERERR</h1>
+    
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+          collapsed: true
+        };
+      }
+    
+      toggleNavbar() {
+        this.setState({
+          collapsed: !this.state.collapsed
+        });
+      }
+      render() {
+        return (
+          <div>
+            <Navbar color="danger" light>
+              <NavbarBrand href="/" className="text-white mr-auto">Moore Properties</NavbarBrand>
+              <div className="text-white mr-4">Hello Howie!</div>
+              <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+              <Collapse isOpen={!this.state.collapsed} navbar>
+                <Nav navbar>
+                  <NavItem>
+                    <NavLink href="./properties">Properties</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="./invoices">Invoices</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="./maintenance">Maintenance</NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </div>
         );
+      }
     }
-}
