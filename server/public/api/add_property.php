b7e441f4b05806ea9b7a2ebcc5866d29c3569c0b
Howard <qsshorty@gmail.com>
2019-06-03 21:37:59 +0000

    Changed from Get to Post

--- server/public/api/add_property.php
+++ server/public/api/add_property.php
@@ -2,18 +2,22 @@
 require_once('../../credentials.php');
 require_once('functions.php');
 startup();
-$pName = $GET_(property_name);
-$street = $GET_(street_address);
-$city = $GET_(city);
-$state = $GET_(state);
-$zip = $GET_(zip);
-$sqft = $GET_(sqft);
-$type = $GET_(property_type);
-$cName = $GET_(manager_contact);
-$phone = $GET_(manager_phone);
-$park = $GET_(parking_spaces);
+$pName = $_POST[property_name];
+$street = $_POST[street_address];
+$city = $_POST[city];
+$state = $_POST[state];
+$zip = $_POST[zip];
+$sqft = $_POST[sqft];
+$type = $_POST[property_type];
+$cName = $_POST[manager_contact];
+$phone = $_POST[manager_phone];
+$park = $_POST[parking_spaces];
 $query= "INSERT INTO `properties`(`property_name`, `street_address`, `city`, `state`, `zip`, `sqft`, `property_type`, `manager_contact`, `manager_phone`, `parking_spaces`) 
-VALUES ($pName, $street, $city, $state, $zip, $sqft, $type, $cName, $phone, $park
+VALUES ($pName, $street, $city, $state, $zip, $sqft, $type, $cName, $phone, $park)";
 
-)";
+$result = mysqli_query($conn, $query);
+
+
+
+
 
