9cf1846323811d86b35bdf426069a6631451166e
Howard <qsshorty@gmail.com>
2019-06-03 21:34:01 +0000

    finished add unit

---
+++ server/public/api/add_unit.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('../../credentials.php');
+require_once('functions.php');
+startup();
+
+$pID = $_POST['property_id'];
+$unitNum = $_POST['unit_number'];
+$sqft = $_POST['sqft'];
+$rent = $_POST['rent'];
+$status = $_POST['status'];
+
+$query = "INSERT INTO `units` (`property_id`, `unit_number`, `sqft`, `rent`, `status`)
+VALUES ('$pID', '$unitNum', '$sqft', '$rent', '$status')";
+
+$result = mysqli_query($conn, $query);
+
+
