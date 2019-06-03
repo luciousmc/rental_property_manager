9c735ca3ff2a1328b3ccb45432623a2611c88b7c
Marlon Clay <luciousmc@icloud.com>
2019-06-03 22:13:27 +0000

    moved views to their own files

---
+++ client/components/add-property.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export const AddProperty = ()=>(
+    <React.Fragment>
+        <Row>                            
+            <Header/> 
+        </Row>
+        <Row>
+            <Container>
+                <div className="form-container col-8 offset-2">
+                    <AddProperty />
+                </div>
+            </Container>
+        </Row>
+    </React.Fragment>
+    );
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from './header';
 import { Container, Row, Col, Card, CardBody, CardTitle, CardImg, Button } from 'reactstrap';
-import PropertyList from './property_list';
-import AddProperty from './addProperty';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { LandingPage } from './landing';
+import  ManagerMain from './manager-main';
+import  AddProperty  from './add-property';
+import  TenantMain from './tenant-main';
+// import PropertyDetails from '../page/PropertyDetails';
 
 export default class App extends React.Component{
     constructor(props) {
@@ -14,72 +18,26 @@ export default class App extends React.Component{
             },
             properties: {}
         }
-        this.setView = this.setView.bind(this);
     }
     setView( view, obj ){
         this.setState({ view:{ name: view, params: obj} });
     }
     render(){
-        if (this.state.view.name === 'landing'){
-            return(
-                <React.Fragment>
-                    <Row>
-                        <Header/> 
-                    </Row>
-                    <Container>
-                        <Row className="col-8 offset-2">
-                            <Col className="mr-auto">
-                                <Card style={{ width: '18rem' }}>
-                                    <CardImg  width="100%" src="images/manager_icon.png" />
-                                    <CardBody>
-                                        <CardTitle>Property Manager</CardTitle>
-                                        <Button onClick={()=>this.setView('manager-main', {})}>Go to Portal</Button>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col width="50%">
-                            <Card className="mr-auto" style={{ width: '18rem' }}>
-                                    <CardImg width="100%" src="images/tenant_icon.png" />
-                                    <CardBody>
-                                        <CardTitle>Tenant</CardTitle>
-                                        <Button>Go to Portal</Button>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>
-                </React.Fragment>
-            );
-        } else if (this.state.view.name === 'add-property'){
-            return(
-                <React.Fragment>
-                    <Row>                            
-                        <Header/> 
-                    </Row>
-                    <Row>
-                        <Container>
-                            <div className="form-container col-8 offset-2">
-                                <AddProperty />
-                            </div>
-                        </Container>
-                    </Row>
-                </React.Fragment>
-            );
-        } else if (this.state.view.name === 'manager-main'){
-            return(
-                <React.Fragment>
-                    <Row>
-                        <Header/> 
-                    </Row>
-                    <Container>
-                        <Row>
-                            <Col className="col-8 offset-2">
-                                <PropertyList setView={this.setView} />
-                            </Col>
-                        </Row>
-                    </Container>
-                </React.Fragment>
-            );
-        }
+        return(
+            <React.Fragment>
+                <Row>
+                    <Header/> 
+                </Row>
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component ={LandingPage} />
+                        <Route path="/manager/main" component ={ManagerMain} />
+                        {/* <Route path="/manager/property-details" component ={PropertyDetails} /> */}
+                        <Route path="/manager/add-property" component ={AddProperty} />
+                        <Route path="/tenant/main" component ={TenantMain} />                        
+                    </Switch>
+                </Router>
+            </React.Fragment>
+        );
     }
 }
