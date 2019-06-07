789e71beb03fc07d94018a07114f25489f6632d2
Marlon Clay <luciousmc@icloud.com>
2019-06-07 18:50:01 +0000

    removed duplicate path for add property

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -9,8 +9,6 @@ import PropertyDetailPage from './property-detail-page';
 import  AddProperty  from './add-property-page';
 import  TenantMain from './tenant-main-page';
 import TenantOwner from './tenant-owner-page';
-// import PropertyDetails from '../page/PropertyDetails';
-import PropertyList from './property_list';
 import AddUnit from './add-unit-page';
 
 export default class App extends React.Component{
@@ -32,7 +30,6 @@ export default class App extends React.Component{
                         <Route path="/manager-main" component={ManagerMain} />
                         <Route path="/property/:id" component ={PropertyDetailPage} />
                         <Route path="/add-property" component ={AddProperty} />
-                        {/* <Route path="/property/1/add-unit" component ={AddUnit} /> */}
                         <Route path="/tenant-main" component ={TenantMain} />                    
                         <Route path="/add-unit/:id" component ={AddUnit} />
                         <Route path="/tenant-owner" component ={TenantOwner} />
