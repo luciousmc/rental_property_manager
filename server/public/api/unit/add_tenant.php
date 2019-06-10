603dba84d44580741ef993ed0037b7e73b4277c7
chrisg91322 <chrisg913@gmail.com>
2019-06-10 20:16:20 +0000

    new tenants displaying in db

--- server/public/api/unit/add_tenant.php
+++ server/public/api/unit/add_tenant.php
@@ -3,22 +3,19 @@ require_once('../credentials.php');
 require_once('../functions.php');
 startup();
 $input = file_get_contents('php://input');
-$_POST = json_decode($input, true);
+$tenant = json_decode($input, true);
 
-// $tenantID = $_POST['tenant_id'];
-$unitID = $_POST['unit_id'];
-$businessName = $_POST['business_name'];
-$contactName = $_POST['contact_name'];
-$tenantPhone = $_POST['tenant_phone'];
-$tenantEmail = $_POST['tenant_email'];
-$moveInDate = $_POST['move_in_date'];
-$leaseEndDate = $_POST['lease_end_date'];
-$rentDueDate = $_POST['rent_due_date'];
-$rent = $_POST['rent'];
+$businessName = $tenant['business_name'];
+$contactName = $tenant['contact_name'];
+$tenantPhone = $tenant['tenant_phone'];
+$tenantEmail = $tenant['tenant_email'];
+$moveInDate = $tenant['move_in_date'];
+$leaseEndDate = $tenant['lease_end_date'];
+$rentDueDate = $tenant['rent_due_date'];
 
-$query = "INSERT INTO `tenants` (`tenant_id`, `unit_id`, `business_name`, `contact_name`, `tenant_phone`, `tenant_email`, `move_in_date`, `lease_end_date`, `rent_due_date`, `rent`)
-VALUES ('$unitID', '$businessName', '$contactName', '$tenantPhone', '$tenantEmail', '$moveInDate', '$leaseEndDate', '$rentDueDate', '$rent')";
-// print($query);
+
+$query = "INSERT INTO `tenants` ( `business_name`, `contact_name`, `tenant_phone`, `tenant_email`, `move_in_date`, `lease_end_date`, `rent_due_date`)
+                        VALUES ('$businessName', '$contactName', '$tenantPhone', '$tenantEmail', '$moveInDate', '$leaseEndDate', '$rentDueDate')";
 $result = mysqli_query($conn, $query);
 if(!$result){
     print(mysqli_error($conn));
