73b44cac865d19abf097b919eb16d277c77ae938
Howard <qsshorty@gmail.com>
2019-06-13 17:00:06 +0000

    server deploy setup

--- server/public/api/image_upload.php
+++ server/public/api/image_upload.php
@@ -3,14 +3,15 @@ require_once('credentials.php');
 require_once('functions.php');
 
 $target_dir = "uploads/";
-$file_path = $target_dir . basename($_FILES["imageToUpload"]["name"]);
+$file_path = $target_dir . basename($_FILES["imageToUpload"]["tmp_name"]);
+$file_name = $target_dir . basename($_FILES["imageToUpload"]["name"]);
 $property = $_POST['property_id'];
-$path = '/api/' .$file_path;
+$path = '/api/' .$file_name;
 $uploadOk = 1;
 if (!(is_dir($target_dir))) {
     mkdir($target_dir);
 }
-$imageFileType = strtolower(pathinfo($file_path,PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($file_name,PATHINFO_EXTENSION));
 
 //if(isset($_POST["submit"])) {
 //    $check = getimagesize($_FILES["imageToUpload"]["tmp_name"]);
@@ -44,7 +45,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 
 } else {
-    if (move_uploaded_file($_FILES["imageToUpload"]["tmp_name"], $file_path)) {
+    if (move_uploaded_file($_FILES["imageToUpload"]["tmp_name"], $file_name)) {
         $query = "INSERT INTO `property_images`(`property_id`, `url`) VALUES ('$property','$path')";
         $result = mysqli_query($conn, $query);
         echo "The file ". basename( $_FILES["imageToUpload"]["tmp_name"]). " has been uploaded.";
